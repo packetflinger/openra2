ee28e0c9a9f3fe35710e605a7d92c0831d8f101f
Joe Reid <joe@joereid.com>
2018-06-14 21:41:37 -0400

    added wildcard match function for player names

--- g_local.h
+++ g_local.h
@@ -850,6 +850,8 @@ char    *vtos(vec3_t v);
 float vectoyaw(vec3_t vec);
 void vectoangles(vec3_t vec, vec3_t angles);
 arena_t *FindArena(edict_t *ent);
+qboolean match(char *pattern, char *haystack);
+
 //
 // g_combat.c
 //
