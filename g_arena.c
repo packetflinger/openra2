34cdeacbe8a6325f7dccad8a51fa4d41f54f03d0
Joe Reid <joe@joereid.com>
2019-05-12 19:09:55 -0400

    much cleaner countdowns

--- g_arena.c
+++ g_arena.c
@@ -1036,79 +1036,21 @@ void G_CheckTimers(arena_t *a) {
 	
 	if (a->state == ARENA_STATE_COUNTDOWN) {
 		remaining = (a->round_start_frame - level.framenum) * FRAMETIME;
-		switch (remaining) {
-		case 10:
-			G_ArenaSound(a, level.sounds.countdown[10]);
-			break;
-		case 9:
-			G_ArenaSound(a, level.sounds.countdown[9]);
-			break;
-		case 8:
-			G_ArenaSound(a, level.sounds.countdown[8]);
-			break;
-		case 7:
-			G_ArenaSound(a, level.sounds.countdown[7]);
-			break;
-		case 6:
-			G_ArenaSound(a, level.sounds.countdown[6]);
-			break;
-		case 5:
-			G_ArenaSound(a, level.sounds.countdown[5]);
-			break;
-		case 4:
-			G_ArenaSound(a, level.sounds.countdown[4]);
-			break;
-		case 3:
-			G_ArenaSound(a, level.sounds.countdown[3]);
-			break;
-		case 2:
-			G_ArenaSound(a, level.sounds.countdown[2]);
-			break;
-		case 1:
-			G_ArenaSound(a, level.sounds.countdown[1]);
-			break;
-		case 0:
+
+		if (remaining > 0 && remaining <= 10) {
+			G_ArenaSound(a, level.sounds.countdown[remaining]);
+		} else if (remaining == 0) {
 			G_ArenaSound(a, level.sounds.fight[1]);
-			break;
 		}
 	}
 
 	if (a->state == ARENA_STATE_TIMEOUT) {
 		remaining = (a->timein_frame - level.framenum) * FRAMETIME;
-		switch (remaining) {
-		case 10:
-			G_ArenaSound(a, level.sounds.countdown[10]);
-			break;
-		case 9:
-			G_ArenaSound(a, level.sounds.countdown[9]);
-			break;
-		case 8:
-			G_ArenaSound(a, level.sounds.countdown[8]);
-			break;
-		case 7:
-			G_ArenaSound(a, level.sounds.countdown[7]);
-			break;
-		case 6:
-			G_ArenaSound(a, level.sounds.countdown[6]);
-			break;
-		case 5:
-			G_ArenaSound(a, level.sounds.countdown[5]);
-			break;
-		case 4:
-			G_ArenaSound(a, level.sounds.countdown[4]);
-			break;
-		case 3:
-			G_ArenaSound(a, level.sounds.countdown[3]);
-			break;
-		case 2:
-			G_ArenaSound(a, level.sounds.countdown[2]);
-			break;
-		case 1:
-			G_ArenaSound(a, level.sounds.countdown[1]);
-			break;
-		case 0:
+
+		if (remaining > 0 && remaining <= 10) {
+			G_ArenaSound(a, level.sounds.countdown[remaining]);
+		} else if (remaining == 0) {
 			G_ArenaSound(a, level.sounds.timein);
-			break;
 		}
 	}
 }
