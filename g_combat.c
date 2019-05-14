1aaa8c83cf82934bdc256d3dd227e97f3cf9e0ec
Joe Reid <joe@joereid.com>
2019-05-14 13:15:18 -0400

    fixes the crash on issue #13 gibbing leftover bodies no longer causes a crash, but perhaps they should still be cleaned up between matches

--- g_combat.c
+++ g_combat.c
@@ -100,7 +100,7 @@ static void Killed(edict_t *targ, edict_t *inflictor, edict_t *attacker, int dam
     targ->enemy = attacker;
     targ->die(targ, inflictor, attacker, damage, point);
 	
-	if (ARENA(targ)->state > ARENA_STATE_WARMUP) {
+	if (ARENA(attacker)->state > ARENA_STATE_WARMUP) {
 		SetChaseTarget(targ, attacker);
 	}
 }
