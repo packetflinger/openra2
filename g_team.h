2c860e96f4249d513c9990c70b175d47be0a9602
Joe Reid <joe@joereid.com>
2024-05-28 22:40:58 -0400

    Revert "circular dependencies are killing me, revert this"
    This reverts commit 0c85280366996090ba30657281987a0bf2a82cbe.

--- g_team.h
+++ g_team.h
@@ -1,8 +1,3 @@
-
-#include "g_local.h"
-
 #pragma once
 
-arena_team_t *FindTeam(arena_t *a, arena_team_type_t type);
-void G_TeamJoin(edict_t *ent, arena_team_type_t type, qboolean forced);
 void G_TeamPart(edict_t *ent, qboolean silent);
