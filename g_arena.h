e8a3f5b9bdecc5c70e55db0dd6c54b9bccb2a9da
Joe Reid <joe@joereid.com>
2024-06-04 09:11:16 -0400

    Rename ARENA_STATE_ROUNDPAUSE to *_RINTERMISSION

--- g_arena.h
+++ g_arena.h
@@ -111,7 +111,7 @@ typedef enum {
     ARENA_STATE_PLAY,
     ARENA_STATE_OVERTIME,
     ARENA_STATE_TIMEOUT,
-    ARENA_STATE_ROUNDPAUSE,        // the gap between rounds in a match
+    ARENA_STATE_RINTERMISSION,  // between rounds
     ARENA_STATE_INTERMISSION,
 } arena_state_t;
 
