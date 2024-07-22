69532aae53123494fab641a0f663f0e23e9d3574
Joe Reid <joe@joereid.com>
2024-07-21 22:27:32 -0400

    Formatting for g_random.c

--- g_random.c
+++ g_random.c
@@ -53,9 +53,10 @@
 static unsigned long mt[N]; /* the array for the state vector  */
 static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
 
-/* initializes mt[N] with a seed */
-void init_genrand(unsigned long s)
-{
+/**
+ * initializes mt[N] with a seed
+ */
+void init_genrand(unsigned long s) {
     mt[0]= s & 0xffffffffUL;
     for (mti=1; mti<N; mti++) {
         mt[mti] = 
@@ -69,47 +70,59 @@ void init_genrand(unsigned long s)
     }
 }
 
-/* initialize by an array with array-length */
-/* init_key is the array for initializing keys */
-/* key_length is its length */
-/* slight change for C++, 2004/2/26 */
-void init_by_array(unsigned long init_key[], int key_length)
-{
+/**
+ * Initialize by an array with array-length
+ *
+ * init_key is the array for initializing keys
+ * key_length is its length
+ *
+ * slight change for C++, 2004/2/26
+ */
+void init_by_array(unsigned long init_key[], int key_length) {
     int i, j, k;
     init_genrand(19650218UL);
     i=1; j=0;
     k = (N>key_length ? N : key_length);
     for (; k; k--) {
         mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
-          + init_key[j] + j; /* non linear */
-        mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+          + init_key[j] + j;    // non linear
+        mt[i] &= 0xffffffffUL;  // for WORDSIZE > 32 machines
         i++; j++;
-        if (i>=N) { mt[0] = mt[N-1]; i=1; }
-        if (j>=key_length) j=0;
+        if (i>=N) {
+            mt[0] = mt[N-1];
+            i=1;
+        }
+        if (j>=key_length) {
+            j=0;
+        }
     }
     for (k=N-1; k; k--) {
         mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
           - i; /* non linear */
         mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
         i++;
-        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+        if (i>=N) {
+            mt[0] = mt[N-1];
+            i=1;
+        }
     }
 
     mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
 }
 
-/* generates a random number on [0,0xffffffff]-interval */
-unsigned long genrand_int32(void)
-{
+/**
+ * Generates a random number on [0,0xffffffff]-interval
+ */
+unsigned long genrand_int32(void) {
     unsigned long y;
     static unsigned long mag01[2]={0x0UL, MATRIX_A};
-    /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
     if (mti >= N) { /* generate N words at one time */
         int kk;
 
-        if (mti == N+1)   /* if init_genrand() has not been called, */
-            init_genrand(5489UL); /* a default initial seed is used */
+        if (mti == N+1) {           // if init_genrand() has not been called,
+            init_genrand(5489UL);   // a default initial seed is used
+        }
 
         for (kk=0;kk<N-M;kk++) {
             y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
@@ -136,24 +149,25 @@ unsigned long genrand_int32(void)
     return y;
 }
 
-/* generates a random number on [0,0x7fffffff]-interval */
-long genrand_int31(void)
-{
+/**
+ * Generates a random number on [0,0x7fffffff]-interval
+ */
+long genrand_int31(void) {
     return (long)(genrand_int32()>>1);
 }
 
-/* generates a random number on [0,1]-real-interval */
-double genrand_float32_full(void)
-{
-    return genrand_int32()*(1.0/4294967295.0); 
-    /* divided by 2^32-1 */ 
+/**
+ * Generates a random number on [0,1]-real-interval
+ */
+double genrand_float32_full(void) {
+    return genrand_int32()*(1.0/4294967295.0);  // divided by 2^32-1
 }
 
-/* generates a random number on [0,1)-real-interval */
-double genrand_float32_notone(void)
-{
-    return genrand_int32()*(1.0/4294967296.0); 
-    /* divided by 2^32 */
+/**
+ * Generates a random number on [0,1)-real-interval
+ */
+double genrand_float32_notone(void) {
+    return genrand_int32()*(1.0/4294967296.0); // divided by 2^32
 }
 
 /* These real versions are due to Isaku Wada, 2002/01/09 added */
