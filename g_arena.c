a53c8263475319473ed6e57cb6e4b08679faf0b4
Joe Reid <joe@joereid.com>
2024-10-17 14:00:04 -0400

    input check for G_ShowScores()

--- g_arena.c
+++ g_arena.c
@@ -1636,7 +1636,10 @@ char *G_RoundToString(arena_t *a) {
 void G_ShowScores(arena_t *a) {
     uint8_t i, j;
     edict_t *ent;
-
+    if (!a) {
+        gi.dprintf("%s(): null arena\n", __func__);
+        return;
+    }
     // for each team
     for (i = 0; i < a->team_count; i++) {
         // for each player
